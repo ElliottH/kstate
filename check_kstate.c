067fc2cf5c64a15c7f20890a1a948272f3ed4f03
Tony Ibbs <tibs@kynesim.co.uk>
2013-01-30 10:43:33 +0000

    Look - a failing test to be mended
    Aborting a transaction should do what it says on the box.

--- check_kstate.c
+++ check_kstate.c
@@ -345,22 +345,6 @@ START_TEST(writing_state_pointer_fails) // expect signal SIGSEGV
 }
 END_TEST
 
-// XXX At the moment, it is allowed to subscribe for WRITE, although
-// XXX this is shorthand for READ|WRITE. Some decision needs to be made
-// XXX about whether this laziness is good or bad.
-START_TEST(subscribe_for_write_and_unsubscribe)
-{
-  char *state_name = kstate_get_unique_name("Fred");
-  kstate_state_p state = kstate_new_state();
-  int rv = kstate_subscribe_state(state, state_name, KSTATE_WRITE);
-  free(state_name);
-  ck_assert_int_eq(rv, 0);
-
-  kstate_unsubscribe_state(state);
-  kstate_free_state(&state);
-}
-END_TEST
-
 START_TEST(subscribe_for_write_then_for_read)
 {
   char *state_name = kstate_get_unique_name("Fred");
@@ -1235,6 +1219,126 @@ START_TEST(interleaved_transactions_same_state_abort_commit)
 }
 END_TEST
 
+START_TEST(write_to_readonly_transaction_fails) // expect signal SIGSEGV
+{
+  char *state_name = kstate_get_unique_name("Fred");
+  kstate_state_p state = kstate_new_state();
+  int rv = kstate_subscribe_state(state, state_name, KSTATE_WRITE);
+  free(state_name);
+  ck_assert_int_eq(rv, 0);
+
+  kstate_transaction_p transaction = kstate_new_transaction();
+  rv = kstate_start_transaction(transaction, state, KSTATE_READ);
+  ck_assert_int_eq(rv, 0);
+
+  uint32_t *ptr = kstate_get_transaction_ptr(transaction);
+  *ptr = 0x12345678;
+
+  kstate_free_state(&state);
+  fail_unless(state == NULL);
+
+  rv = kstate_commit_transaction(transaction);
+  ck_assert_int_eq(rv, 0);
+  fail_if(kstate_transaction_is_active(transaction));
+
+  kstate_free_transaction(&transaction);
+}
+END_TEST
+
+START_TEST(write_to_writeable_transaction_does_not_fail)
+{
+  char *state_name = kstate_get_unique_name("Fred");
+  kstate_state_p state = kstate_new_state();
+  int rv = kstate_subscribe_state(state, state_name, KSTATE_WRITE);
+  free(state_name);
+  ck_assert_int_eq(rv, 0);
+
+  kstate_transaction_p transaction = kstate_new_transaction();
+  rv = kstate_start_transaction(transaction, state, KSTATE_WRITE);
+  ck_assert_int_eq(rv, 0);
+
+  uint32_t *t_ptr = kstate_get_transaction_ptr(transaction);
+  *t_ptr = 0x12345678;
+
+  rv = kstate_abort_transaction(transaction);
+  ck_assert_int_eq(rv, 0);
+
+  kstate_free_transaction(&transaction);
+
+  kstate_free_state(&state);
+  fail_unless(state == NULL);
+}
+END_TEST
+
+START_TEST(write_to_writeable_transaction_visible_after_commit)
+{
+  char *state_name = kstate_get_unique_name("Fred");
+  kstate_state_p state = kstate_new_state();
+  int rv = kstate_subscribe_state(state, state_name, KSTATE_WRITE);
+  free(state_name);
+  ck_assert_int_eq(rv, 0);
+
+  kstate_transaction_p transaction = kstate_new_transaction();
+  rv = kstate_start_transaction(transaction, state, KSTATE_WRITE);
+  ck_assert_int_eq(rv, 0);
+
+  uint32_t *t_ptr = kstate_get_transaction_ptr(transaction);
+  *t_ptr = 0x12345678;
+
+  rv = kstate_commit_transaction(transaction);
+  ck_assert_int_eq(rv, 0);
+
+  kstate_free_transaction(&transaction);
+
+  uint32_t *s_ptr = kstate_get_state_ptr(state);
+  ck_assert_int_eq(rv, 0);
+
+  ck_assert_int_eq(*s_ptr, 0x12345678);
+
+  kstate_free_state(&state);
+  fail_unless(state == NULL);
+}
+END_TEST
+
+START_TEST(write_to_writeable_transaction_not_visible_after_abort)
+{
+  char *state_name = kstate_get_unique_name("Fred");
+  kstate_state_p state = kstate_new_state();
+  int rv = kstate_subscribe_state(state, state_name, KSTATE_WRITE);
+  free(state_name);
+  ck_assert_int_eq(rv, 0);
+
+  // As we remember, the mapped area starts off all zeroes
+  uint32_t *s_ptr1 = kstate_get_state_ptr(state);
+  ck_assert_int_eq(rv, 0);
+  ck_assert_int_eq(*s_ptr1, 0);
+
+  kstate_transaction_p transaction = kstate_new_transaction();
+  rv = kstate_start_transaction(transaction, state, KSTATE_WRITE);
+  ck_assert_int_eq(rv, 0);
+
+  uint32_t *t_ptr = kstate_get_transaction_ptr(transaction);
+  *t_ptr = 0x12345678;
+
+  rv = kstate_abort_transaction(transaction);
+  ck_assert_int_eq(rv, 0);
+
+  kstate_free_transaction(&transaction);
+
+  uint32_t *s_ptr2 = kstate_get_state_ptr(state);
+  ck_assert_int_eq(rv, 0);
+
+  // The state still has the same location mapped
+  fail_unless(s_ptr1 == s_ptr2);
+
+  // The state does not see the uncommitted change
+  ck_assert_int_eq(*s_ptr2, 0);
+
+  kstate_free_state(&state);
+  fail_unless(state == NULL);
+}
+END_TEST
+
 Suite *test_kstate_suite(void)
 {
   Suite *s = suite_create("Kstate");
@@ -1266,7 +1370,6 @@ Suite *test_kstate_suite(void)
   tcase_add_test(tc_core, query_state_pointer);
   tcase_add_test(tc_core, can_read_state_pointer);
   tcase_add_test_raise_signal(tc_core, writing_state_pointer_fails, SIGSEGV);
-  tcase_add_test(tc_core, subscribe_for_write_and_unsubscribe);
   tcase_add_test(tc_core, subscribe_for_write_then_for_read);
   tcase_add_test(tc_core, subscribe_for_write_then_for_write);
   tcase_add_test(tc_core, subscribe_for_write_then_for_read_unsubscribe_other_order);
@@ -1307,6 +1410,10 @@ Suite *test_kstate_suite(void)
   tcase_add_test(tc_core, interleaved_transactions_same_state_commit_commit);
   tcase_add_test(tc_core, interleaved_transactions_same_state_commit_abort);
   tcase_add_test(tc_core, interleaved_transactions_same_state_abort_commit);
+  tcase_add_test_raise_signal(tc_core, write_to_readonly_transaction_fails, SIGSEGV);
+  tcase_add_test(tc_core, write_to_writeable_transaction_does_not_fail);
+  tcase_add_test(tc_core, write_to_writeable_transaction_visible_after_commit);
+  tcase_add_test(tc_core, write_to_writeable_transaction_not_visible_after_abort);
   // END TESTS
   suite_add_tcase(s, tc_core);
 
